5f9c801710355e516ab0ca28edc1e7a6d28c9b4e
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-18 01:27:46 -0600

    404 Not Found

--- src/app/shared/components/no-found/no-found.component.ts
+++ src/app/shared/components/no-found/no-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   templateUrl: './no-found.component.html',
-  styleUrls: ['./no-found.component.scss']
+  styleUrls: ['./no-found.component.scss'],
 })
 export class NoFoundComponent {
+  constructor(private location: Location) {}
 
+  back(): void {
+    this.location.back();
+  }
 }
